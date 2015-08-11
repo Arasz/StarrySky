d0dce4895334ca103802d33dc51ff8f4b8541c15
Rafal <rafal.araszkiewicz@student.put.poznan.pl>
2015-08-11 15:26:25 +0200

    First commit.

---
+++ StarrySky/ViewModel/BeeStarViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarrySky.ViewModel
+{
+    class BeeStarViewModel
+    {
+        public object PlayAreaSize { get; internal set; }
+    }
+}
